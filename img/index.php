6b6f63165c44ee82b447e0563cc3b8fe2446a140
diewland <diewland@gmail.com>
2022-09-08 22:45:25 +0700

    add verified icon

--- img/index.php
+++ img/index.php
@@ -5,6 +5,7 @@
   $keys = array(
     array('tier', 'basic'),
     array('score', 0),
+    array('v', 0),
   );
   foreach ($keys as $row) {
     [$key, $default] = $row;
@@ -12,6 +13,7 @@
     $$key = $value;
   }
   $score += 0;
+  $v += 0;
 
   // load img
   $path = "./pass_$tier.png";
@@ -22,6 +24,12 @@
   $color = imagecolorallocate($img, 0, 0, 0);
   $font_path = "PressStart2P-Regular.ttf";
   imagettftext($img, 30, 0, 10, 50, $color, $font_path, $text);
+
+  // print verified
+  if ($v == 1) {
+    $ico = imagecreatefrompng('./ico_verified.png');
+    imagecopyresampled($img, $ico, 435, 430, 0, 0, 75, 75, 75, 75);
+  }
 
   // send to browser & release memory
   imagepng($img);

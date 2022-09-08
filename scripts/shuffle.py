36183de63f1c20dfa841b3365e699621dfa3d729
diewland <diewland@gmail.com>
2022-09-08 12:55:43 +0700

    add raffle script

---
+++ scripts/shuffle.py
@@ -0,0 +1,32 @@
+import random
+from datetime import datetime
+
+TITLE = "Jigsaw Pass Raffle #1 Test Drive"
+
+FROM_ID = 1
+TO_ID = 262
+
+SUPPLY = TO_ID - FROM_ID + 1
+URL = "https://qx.app/asset/0x49Bb981c8b721B9873093f519337329f794E8577/{}"
+LOG_PATT = "Round {:03} => {}"
+
+# basic info
+print(TITLE)
+print("Raffle Time: {}".format(datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
+print("SUPPLY: {} ({}-{})".format(SUPPLY, FROM_ID, TO_ID))
+print("")
+
+# init data
+bulk = [ id for id in range(FROM_ID, TO_ID+1) ]
+
+# loop
+for no in range(SUPPLY):
+    print(LOG_PATT.format(no, bulk))
+    if (len(bulk) == 1):
+        winner_id = bulk[0]
+        print("")
+        print("Winner is {} 🎉".format(winner_id))
+        print(URL.format(winner_id))
+    else:
+        bulk = bulk[1:]
+        random.shuffle(bulk)

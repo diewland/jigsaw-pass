4112b0bdd5f8b92f68fb8bfba91eb7cfe9f9d7cb
diewland <diewland@gmail.com>
2022-09-08 14:12:36 +0700

    shuffle testdrive 2

--- scripts/shuffle.py
+++ scripts/shuffle.py
@@ -2,28 +2,31 @@ import random
 from datetime import datetime
 
 TITLE = "Jigsaw Pass Raffle #1 Test Drive"
-DESC = "..."
+DESC = "1x Movie Ticket from MajorVerse"
 
 FROM_ID = 1
 TO_ID = 262
+SKIP_IDS = [ 179 ]
 
-SUPPLY = TO_ID - FROM_ID + 1
 URL = "https://qx.app/asset/0x49Bb981c8b721B9873093f519337329f794E8577/{}"
 LOG_PATT = "Round {:03} => {}"
+
+# init data
+bulk = [ id for id in range(FROM_ID, TO_ID+1) ]
+bulk = list(set(bulk) - set(SKIP_IDS))
+supply = len(bulk)
 
 # basic info
 print(TITLE)
 print(DESC)
 print("")
 print("Raffle Time: {}".format(datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
-print("SUPPLY: {} ({}-{})".format(SUPPLY, FROM_ID, TO_ID))
+print("Supply: {} ({}-{})".format(supply, FROM_ID, TO_ID))
+print("Skip IDs: {}".format(SKIP_IDS))
 print("")
 
-# init data
-bulk = [ id for id in range(FROM_ID, TO_ID+1) ]
-
 # loop
-for no in range(SUPPLY):
+for no in range(supply):
     print(LOG_PATT.format(no, bulk))
     if (len(bulk) == 1):
         winner_id = bulk[0]
